a1b60de1642d36d064e5abcee534761909cfb14e
Chimq12 <tinhbu0123@gmail.com>
2020-06-01 21:58:44 +0700

    Login(chưa hoàn thiện)

--- sign_in/js.js
+++ sign_in/js.js
@@ -1,35 +1,58 @@
 
-function login (arr){
-    let submit= document.getElementById("submit");
-    submit.addEventListener("click", (e) =>{
-        e.preventDefault();
-   let name= document.getElementById("id").value
-   let pass= document.getElementById("pass").value
-      for(let i =0;i< arr.length;i++){
-         if( name===arr[i].username && pass === arr[i].password){
-         alert("ok");
-    return true;
-}
-         else{
-          continue;
-            }
-        }
-alert("gg");
-});
-    }
-let getData = async() =>{
-    let data = await fetch("https://5ed1024f4e6d7200163a045c.mockapi.io/api/c4e/users?fbclid=IwAR30m_KStYj2WRceoB0enVRbOpaW2h1QjQDoHSbgUbBQX3W-TvOoFBgoc28");
-    let data1 = await data.json();
-    login(data1);
-}
-getData();
+// function login (arr){
+//     let submit= document.getElementById("submit");
+//     submit.addEventListener("click", (e) =>{
+//         e.preventDefault();
+//    let name= document.getElementById("id").value
+//    let pass= document.getElementById("pass").value
+//       for(let i =0;i< arr.length;i++){
+//          if( name===arr[i].username && pass === arr[i].password){
+//          alert("ok");
+//     return true;
+// }
+//          else{
+//           continue;
+//             }
+//         }
+// alert("gg");
+// });
+//     }
+// let getData = async() =>{
+//     let data = await fetch("https://5ed1024f4e6d7200163a045c.mockapi.io/api/c4e/users?fbclid=IwAR30m_KStYj2WRceoB0enVRbOpaW2h1QjQDoHSbgUbBQX3W-TvOoFBgoc28");
+//     let data1 = await data.json();
+//     login(data1);
+// }
+// getData();
 
-    // let c= "admin";
-    // let d=123;
-    // if(a==c&&b==d)
-    // { 
-    //    alert("Đăng nhập thành công")
-    //   window.location.href = "https://www.w3schools.com/"; 
-    // }
-    // else
-    // alert("Đăng nhập thất bại");
+document.getElementById("submit").addEventListener("click", (e) => {
+    e.preventDefault();
+    let a = document.getElementById("mk").value
+    let b = document.getElementById("xnmk").value
+    if (a === "" || b === "") {
+        alert("Nhập mật khẩu")
+    } else if (a !== b) {
+        alert("Mật khẩu không đúng")
+    }
+    else {
+        alert("Đăng kí thành công")
+        window.location.href = "./sign_in/index.html"
+    }
+});
+
+document.getElementById("submit").addEventListener("click", (e) => {
+    e.preventDefault();
+    let a = document.getElementById("id").value
+    let b = document.getElementById("pass").value
+    let c = "admin";
+    let d = 123;
+    if (a === "" || b === "") {
+        alert("Nhập thông tin")
+    }
+    else if (a == c && b == d) {
+        alert("Đăng nhập thành công")
+        window.location.href = "https://www.youtube.com/?gl=VN"
+    }
+    else {
+        alert("Đăng nhập thất bại")
+    }
+});
